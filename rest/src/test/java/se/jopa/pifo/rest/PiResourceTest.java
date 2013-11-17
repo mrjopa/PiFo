667aaa5cb79c49010a585ec1fbc2c67899d73d73
Johan Palmer <mrjohanpalmer@gmail.com>
2013-11-17 21:51:30 +0100

    PiResourceTest with jason

--- rest/src/test/java/se/jopa/pifo/rest/PiResourceTest.java
+++ rest/src/test/java/se/jopa/pifo/rest/PiResourceTest.java
@@ -6,12 +6,14 @@ package se.jopa.pifo.rest;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
 
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import se.jopa.pifo.core.model.Pi;
 import se.jopa.pyfo.rest.Main;
 import static org.junit.Assert.assertEquals;
 
@@ -51,8 +53,25 @@ public class PiResourceTest {
      */
     @Test
     public void testGetIt() {
-        String responseMsg = target.path("pi").request().get(String.class);
-        System.out.println(responseMsg);
+    	
+        String responseMsgXml = target.path("pi").request(MediaType.APPLICATION_XML).get(String.class);
+        System.out.println(responseMsgXml);
+    
+        String responseMsgJson = target.path("pi").request(MediaType.APPLICATION_JSON).get(String.class);
+        System.out.println(responseMsgJson);
+
+        
+        Pi pi = target.path("pi").request(MediaType.APPLICATION_JSON).get(Pi.class);
+        System.out.println(pi.getIp());
         //assertEquals("Got it!", responseMsg);
+        
+        
+//        Response response = webTarget.request(MediaType.APPLICATION_JSON).get();
+//
+//        if (response.getStatus() == 200) {      
+//           return response.readEntity(JSONObject.class);
+//        }        
+        
+        
     }
 }
